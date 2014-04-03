425a0763c23011a164e776e602399ff787ed54de
Wassim Dhif <wassimdhif@gmail.com>
2014-04-03 20:31:04 +0200

    -First Commit
    Signed-off-by: Wassim Dhif <wassimdhif@gmail.com>

---
+++ Assets/Scripts/hudScript.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class hudScript : MonoBehaviour {
+
+	float playerScore = 0;
+	
+	// Update is called once per frame
+	void Update () {
+		playerScore += Time.deltaTime;
+	}
+
+	public void Score(int amount)
+	{
+		playerScore += amount;
+	}
+
+	void OnGUI()
+	{
+		GUI.Label (new Rect (10, 10, 100, 30), "Score : " + (int)(playerScore * 100));
+	}
+}
